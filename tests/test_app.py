0f774c2d58037752ca83aa85929bd9494efceaf2
kirillShchepin <67071517+kirillShchepin@users.noreply.github.com>
2025-08-07 21:42:39 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Добавляем путь к корню проекта
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi.testclient import TestClient
 from app.main import app
 
@@ -7,7 +13,7 @@ def test_predict_spam():
     response = client.post("/predict", json={"text": "Win a free iPhone!"})
     assert response.status_code == 200
     assert "result" in response.json()
-    assert response.json()["result"] in ["HAM", "SPAM"]  # Проверяем возможные значения
+    assert response.json()["result"] in ["HAM", "SPAM"]
 
 def test_predict_ham():
     response = client.post("/predict", json={"text": "Hello, how are you?"})
