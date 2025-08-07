d3b767c8ceab9611a6d40e6a68eb17e6a30a0fbb
kirillShchepin <67071517+kirillShchepin@users.noreply.github.com>
2025-08-07 20:50:18 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,9 @@
+from fastapi.testclient import TestClient
+from app.main import app
 
+client = TestClient(app)
+
+def test_predict_spam():
+    response = client.post("/predict", json={"text": "Win a free iPhone!"})
+    assert response.status_code == 200
+    assert "result" in response.json()
