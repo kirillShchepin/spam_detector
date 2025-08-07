698bebfbfbbed396ae3f50c2e5be7f5f93528455
kirillShchepin <67071517+kirillShchepin@users.noreply.github.com>
2025-08-07 20:48:49 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+from transformers import pipeline
+import torch  # Явный импорт для работы модели
 
+app = FastAPI()
+
+# Загрузка модели спам-детектора
+spam_model = pipeline(
+    "text-classification",
+    model="mrm8488/bert-tiny-finetuned-sms-spam-detection"
+)
+
+@app.post("/predict")
+def predict(text: str):
+    return {"result": spam_model(text)[0]["label"]}  # "spam" или "ham"
