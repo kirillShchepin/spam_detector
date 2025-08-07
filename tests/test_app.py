1db688997da1fc39eac17818d370eb1eec91fd3a
kirillShchepin <67071517+kirillShchepin@users.noreply.github.com>
2025-08-07 22:29:20 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,11 +8,19 @@ from app.main import app
 client = TestClient(app)
 
 def test_predict_spam():
-    # Теперь отправляем данные в правильном формате
     response = client.post(
         "/predict",
-        json={"text": "Win a free iPhone!"}  # Совпадает с PredictionRequest
+        json={"text": "Win a free iPhone!"}
     )
     assert response.status_code == 200
     assert "result" in response.json()
+    # Теперь проверяем преобразованные метки
     assert response.json()["result"] in ["spam", "ham"]
+    
+def test_predict_ham():
+    response = client.post(
+        "/predict",
+        json={"text": "Hello, how are you?"}
+    )
+    assert response.status_code == 200
+    assert response.json()["result"] == "ham"
