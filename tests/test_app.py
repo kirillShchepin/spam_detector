5c1522d51ae32d012fd658706ebb2b337f2c75cd
kirillShchepin <67071517+kirillShchepin@users.noreply.github.com>
2025-08-10 20:19:01 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,19 +3,13 @@ from app.main import app
 
 client = TestClient(app)
 
-
-def test_predict_spam():
-    response = client.post("/predict", json={"text": "Win a free iPhone!"})
+def test_root():
+    response = client.get("/")
     assert response.status_code == 200
-    json_data = response.json()
-    assert "result" in json_data
-    assert json_data["result"] in ["spam", "ham"]
-    assert 0 <= json_data["confidence"] <= 1
+    assert response.json()["status"] == "ok"
 
-
-def test_predict_ham():
-    response = client.post("/predict", json={"text": "Hello, how are you?"})
+def test_predict():
+    response = client.post("/predict", json={"text": "You win a free prize!"})
     assert response.status_code == 200
-    json_data = response.json()
-    assert json_data["result"] in ["spam", "ham"]
-    assert 0 <= json_data["confidence"] <= 1
+    assert response.json()["result"] in ["spam", "ham"]
+    assert 0 <= response.json()["confidence"] <= 1
